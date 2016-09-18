0a56c7166813349937e135ec7dd8ccfc78d64b65
elmadjian <cadu_elmadjian@yahoo.com.br>
2016-09-18 18:49:44 -0300

    exercicios aula1

---
+++ aula1/linear_regression.py
@@ -0,0 +1,109 @@
+import sys, re
+import matplotlib.pyplot as plt
+import numpy as np
+
+theta_list = []
+x_list = []
+y_list = []
+
+def main():
+    if len(sys.argv) != 2:
+        print("modo de usar: <este_programa> <arquivo_csv>")
+        sys.exit()
+    csv_file = sys.argv[1]
+
+    with open(csv_file, "r") as arquivo:
+        classes = arquivo.readline().split(",")
+        theta_list = [0.0 for i in range(len(classes))]
+        for line in arquivo:
+            values = line.split(",")
+            curr_x = [float(i) for i in values]
+            curr_x[-1] = 1.0
+            x_list.append(curr_x)
+            y_list.append(float(values[-1]))
+
+        #print("x_list:", x_list, "\n\ny_list:", y_list, "\n\ntheta_list:", theta_list)
+
+
+    #batch_gradient_descent(theta_list, x_list, y_list, 0.0005, 0.0000001)
+    #stochastic_gradient_descent(theta_list, x_list, y_list, 0.0005, 0.00001)
+    theta_list = normal_equations(x_list, y_list)
+    print(theta_list)
+    plot(theta_list, x_list, y_list)
+
+
+#--------------------------------
+def J(theta_list, x_list, y_list):
+    sigma = 0
+    for i in range(len(x_list)):
+        sigma += (h_theta(theta_list, x_list[i]) - y_list[i])**2
+    return sigma / 2
+
+
+#--------------------------------
+def h_theta(theta_list, x_list_i):
+    return np.dot(theta_list, x_list_i)
+
+
+#--------------------------------
+def batch_gradient_descent(theta_list, x_list, y_list, alpha, epsilon):
+    J_prev = 0
+    J_curr = J(theta_list, x_list, y_list)
+    count = 0
+    while (abs(J_curr - J_prev) > epsilon):
+        #print(count)
+        count+=1
+        if count > 10000:
+            print("too much iterations")
+            break
+        for j in range(len(theta_list)):
+            sigma = 0
+            for i in range(len(x_list)):
+                h = h_theta(theta_list, x_list[i])
+                sigma += (h - y_list[i]) * x_list[i][j]
+                #print("h>>", h)
+            theta_list[j] = theta_list[j] - alpha * sigma
+        J_prev = J_curr
+        J_curr = J(theta_list, x_list, y_list)
+
+#--------------------------------
+def stochastic_gradient_descent(theta_list, x_list, y_list, alpha, epsilon):
+    J_prev = 0
+    J_curr = J(theta_list, x_list, y_list)
+    count = 0
+    while (abs(J_curr - J_prev) > epsilon):
+        #print(count)
+        count+=1
+        if count > 10000:
+            print("too much iterations")
+            break
+        for j in range(len(theta_list)):
+            for i in range(len(x_list)):
+                diff = (h_theta(theta_list, x_list[i]) - y_list[i])
+                theta_list[j] = theta_list[j] - alpha * diff * x_list[i][j]
+        J_prev = J_curr
+        J_curr = J(theta_list, x_list, y_list)
+
+#--------------------------------
+def normal_equations(x_list, y_list):
+    X = np.array(x_list)
+    y = np.array(y_list)
+    return np.dot(np.dot(np.linalg.inv(np.dot(X.T, X)), X.T), y)
+
+#--------------------------------
+def plot(theta_list, x_list, y_list):
+    new_x_list = [i[0] for i in x_list]
+    plt.plot(new_x_list, y_list, 'ro')
+    #x_list.sort()
+    #for x in x_list:
+    #    print("x:", x, "thetaTx:", np.dot(theta_list, x))
+    plt.plot(new_x_list, [np.dot((theta_list[0], theta_list[1]), (i[0], i[1])) for i in x_list])
+    plt.title("Regressão sobre os dados em 'height.csv'")
+    plt.xlabel("Idade")
+    plt.ylabel("Altura")
+    plt.show()
+
+
+
+if __name__=="__main__":
+    main()
---
+++ logistic_regression.py
@@ -0,0 +1,98 @@
+import sys, re
+import matplotlib.pyplot as plt
+import numpy as np
+
+theta_list = []
+x_list = []
+y_list = []
+
+def main():
+    if len(sys.argv) != 2:
+        print("modo de usar: <este_programa> <arquivo_csv>")
+        sys.exit()
+    csv_file = sys.argv[1]
+
+    with open(csv_file, "r") as arquivo:
+        classes = arquivo.readline().split(",")
+        theta_list = [0.0 for i in range(len(classes))]
+        for line in arquivo:
+            values = line.split(",")
+            curr_x = [float(i) for i in values[:-1]]
+            curr_x.append(1.0)
+            x_list.append(curr_x)
+            y_list.append(1.0) if values[-1].startswith("yes") else y_list.append(0.0)
+
+        #print("x_list:", x_list, "\n\ny_list:", y_list, "\n\ntheta_list:", theta_list)
+
+
+    logistic_regression(theta_list, x_list, y_list, 0.0005, 0.0000001)
+    print(theta_list)
+    plot(theta_list, x_list, y_list)
+
+def logistic_regression(theta_list, x_list, y_list, alpha, epsilon):
+    J_prev = 0
+    J_curr = J(theta_list, x_list, y_list)
+    count = 0
+    while abs(J_curr - J_prev) > epsilon:
+        if count == 10000:
+            print("too much iterations")
+            break
+        count += 1
+        for j in range(len(theta_list)):
+            for i in range(len(x_list)):
+                diff = (h_theta(theta_list, x_list[i]) - y_list[i])
+                theta_list[j] = theta_list[j] - alpha * diff * x_list[i][j]
+        J_prev = J_curr
+        J_curr = J(theta_list, x_list, y_list)
+
+
+#--------------------------------
+def J(theta_list, x_list, y_list):
+    sigma = 0
+    for i in range(len(x_list)):
+        sigma += (h_theta(theta_list, x_list[i]) - y_list[i])**2
+    return sigma / 2
+
+
+#--------------------------------
+def h_theta(theta, x):
+    return 1.0/(1.0 + np.exp(-np.dot(theta, x)))
+
+
+#--------------------------------
+def predict(theta, x, y):
+    return (h_theta(theta, x)**y) * ((1.0-h_theta(theta, x))**(1.0-y))
+
+
+#--------------------------------
+def plot(theta_list, x_list, y_list):
+    new_x_list = [i[0] for i in x_list]
+    new_y_list = [i[1] for i in x_list]
+    hit = 0
+    for i in range(len(y_list)):
+        # if y_list[i] == 1.0 and predict(theta_list, x_list[i], y_list[i]) >= 0.5:
+        #     plt.plot(np.dot(theta_list, x_list[i]), y_list[i], 'go')
+        # elif y_list[i] == 0.0 and predict(theta_list, x_list[i], y_list[i]) >= 0.5:
+        #     plt.plot(np.dot(theta_list, x_list[i]), y_list[i], 'ro')
+        # else:
+        #     plt.plot(np.dot(theta_list, x_list[i]), y_list[i], 'bx')
+        if y_list[i] == 1.0:
+            if predict(theta_list, x_list[i], y_list[i]) >= 0.5:
+                plt.plot(new_x_list[i], new_y_list[i], 'go')
+                hit += 1
+            else:
+                plt.plot(new_x_list[i], new_y_list[i], 'gx')
+        elif y_list[i] == 0.0 :
+            if predict(theta_list, x_list[i], y_list[i]) >= 0.5:
+                plt.plot(new_x_list[i], new_y_list[i], 'ro')
+                hit += 1
+            else:
+                plt.plot(new_x_list[i], new_y_list[i], 'rx')
+    #plt.plot([np.dot(theta_list, i) for i in x_list], [h_theta(theta_list, i) for i in x_list], "yo")
+    print("hit rate:", hit/len(y_list))
+    plt.show()
+
+
+
+if __name__=="__main__":
+    main()
